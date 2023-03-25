91d63f599c1c7a7f4163f1d9ba193d3a420cde95
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-03-25 23:04:34 +0000

    added nextconfig

--- next.config.js
+++ next.config.js
@@ -1,31 +1,33 @@
-require("dotenv").config();
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-// module.exports = () => {
-//   if (phase === PHASE_DEVELOPMENT_SERVER) {
-//     return {
-//       env: {
-//         mongodb_username: process.env.MONGODB_USERNAME,
-//         mongodb_password: process.env.MONGODB_PASSWORD,
-//         mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
-//         mongodb_database: process.env.MONGODB_DATABASE,
-//       },
-//     };
-//   }
-//   return {
-//     env: {
-//       mongodb_username: process.env.MONGODB_USERNAME,
-//       mongodb_password: process.env.MONGODB_PASSWORD,
-//       mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
-//       mongodb_database: process.env.MONGODB_DATABASE,
-//     },
-//   };
-// };
+// require("dotenv").config();
 
-module.exports = {
-  env: {
-    mongodb_username: process.env.MONGODB_USERNAME,
-    mongodb_password: process.env.MONGODB_PASSWORD,
-    mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
-    mongodb_database: process.env.MONGODB_DATABASE,
-  },
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: process.env.MONGODB_USERNAME,
+        mongodb_password: process.env.MONGODB_PASSWORD,
+        mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+        mongodb_database: process.env.MONGODB_DATABASE,
+      },
+    };
+  }
+  return {
+    env: {
+      mongodb_username: process.env.MONGODB_USERNAME,
+      mongodb_password: process.env.MONGODB_PASSWORD,
+      mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+      mongodb_database: process.env.MONGODB_DATABASE,
+    },
+  };
 };
+
+// module.exports = {
+//   env: {
+//     mongodb_username: process.env.MONGODB_USERNAME,
+//     mongodb_password: process.env.MONGODB_PASSWORD,
+//     mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+//     mongodb_database: process.env.MONGODB_DATABASE,
+//   },
+// };
